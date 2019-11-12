2ad43135c44dd58dd557fe8b76eacd1683451ff7
Stella <feelis1119@naver.com>
2019-11-12 16:38:17 -0500

    update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const hbs = require('hbs');
 
 // set the port
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
@@ -13,7 +13,18 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use('/', require('./routes/index'));
 
+app.use((req, res, next) => {
+  var err = new Error('Not Found');
+  err.status = 404;
+  err.customMessage = "Something terrible has happened! Your page has gone missing!"
+
+  next(err);
+})
+
+app.use((err, req, res, next) => {
+  res.render('error', {data: err, layout: 'errorPage'})
+})
+
 app.listen(port, () => {
     console.log(`app is running on ${port}`);
 })
-
